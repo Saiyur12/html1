543b1b1127e6096808304bde5a877bca12e01d5a
Saiyur12 <saiyursingh12@gmail.com>
2025-05-20 18:52:49 +0200

    percentage

---
+++ PYTHON/pythonoperators/avarage.py
@@ -0,0 +1,17 @@
+
+# Storing Values
+
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finding the total sum of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the tree is :", sum)
+
+
+#Finding the avarage of trees
+avarage =sum
+print("the avarage of all the tree is :", avarage)
