17d1399f3b198f6c3f66a37fd720df51202004da
Saiyur12 <saiyursingh12@gmail.com>
2025-06-17 18:48:34 +0200

    drawing

---
+++ PYTHON/turtle/star.py
@@ -0,0 +1,40 @@
+
+import turtle
+
+turtle.Screen().bgcolor("yellow")
+board = turtle.Turtle()
+
+turtle.color("red")
+
+#first triangle for star
+board.forward(100) # draw base
+
+board.left(120)
+board.forward(100)
+
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+#second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+
+
+board.right(120)
+board.forward(100)
+
+
+board.right(120)
+board.forward(100)
+
+
+turtle.done()
+
+
+
