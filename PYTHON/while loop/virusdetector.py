c0c99f6c66ac2a925d85bbd4ab82bf219cd26d7f
Saiyur12 <saiyursingh12@gmail.com>
2025-10-21 18:44:19 +0200

    events.py

---
+++ PYTHON/while loop/virusdetector.py
@@ -0,0 +1,18 @@
+# Import nesessary libaries
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry("200x200")
+
+# Function for Displaying warning message
+# this will be called once the button is clicked
+# messagebox.showwarning
+("Window Name", "Text to be displayed")
+def msg():
+    messagebox.showwarning("Alert", "Stop! Virus Found.")
+
+button = Button(root, text="Scan for Virus", command=msg) 
+button.place(x=40, y=80) 
+
+root.mainloop()
