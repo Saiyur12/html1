9be1db5a9cdbf04a7a4289f1290031a789d7a5e3
Saiyur12 <saiyursingh12@gmail.com>
2025-10-22 18:52:11 +0200

    text and file.py

--- PYTHON/while loop/virusdetector.py
+++ PYTHON/while loop/virusdetector.py
@@ -12,7 +12,11 @@ root.geometry("200x200")
 def msg():
     messagebox.showwarning("Alert", "Stop! Virus Found.")
 
+    
+    
+
 button = Button(root, text="Scan for Virus", command=msg) 
 button.place(x=40, y=80) 
+
 
 root.mainloop()
